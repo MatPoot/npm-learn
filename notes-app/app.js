7f32aa264c862c9f7fe3c5adbdaa83b2f931da1a
MatPoot <ch2shahid@gmail.com>
2019-05-20 11:28:33 -0600

    Chalk and validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
 const name = require('./utils.js');
 const getNotes = require('./note.js');
 const validator = require('validator');
+const chalk = require('chalk');
 
 console.log(name);
 const message = getNotes();
 console.log(message);
+
+console.log(chalk.red(validator.isEmail('test@test.com')));
